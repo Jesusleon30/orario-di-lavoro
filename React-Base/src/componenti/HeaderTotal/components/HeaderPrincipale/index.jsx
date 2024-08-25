6b21c1ae757a9a7e7f44bff9438b029322007a40
Jesus Hugo Retamozo Leon <jesusretamozo@hotmail.it>
2024-08-25 17:30:24 +0200

    update

--- React-Base/src/componenti/HeaderTotal/components/HeaderPrincipale/index.jsx
+++ React-Base/src/componenti/HeaderTotal/components/HeaderPrincipale/index.jsx
@@ -3,6 +3,7 @@ import "./css/index.css";
 
 
 export default function HeaderPrincipale() {
+
   const [menu, setMenu] = useState(false);
 
   const toggleMenu = () => {
@@ -33,7 +34,7 @@ export default function HeaderPrincipale() {
               viewBox="0 0 16 16"
             >
               <path
-                fill-rule="evenodd"
+                fillRule="evenodd"
                 d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
               />
             </svg>

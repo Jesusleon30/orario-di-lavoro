6670ae77be425447b6b0f91abfb379b08b76af01
Jesus Hugo Retamozo Leon <jesusretamozo@hotmail.it>
2024-09-25 22:23:02 +0200

    Alfa Robotica update

--- React-Base/src/componenti/HeaderTotal/components/HeaderPrincipale/index.jsx
+++ React-Base/src/componenti/HeaderTotal/components/HeaderPrincipale/index.jsx
@@ -1,21 +1,13 @@
-import { useState } from "react";
-import "./css/index.css";
 
+import "./css/index.css";
 
 export default function HeaderPrincipale() {
 
-  const [menu, setMenu] = useState(false);
-
-  const toggleMenu = () => {
-    setMenu(!menu);
-  };
-
- 
 
   return (
     <>
-      <section className="">
-        <header className="Cabecera ">
+      <section className=" ">
+        <header className="Cabecera justify-between">
           <button>
             <img
               className="h-[80px] w-[80px]"
@@ -23,26 +15,10 @@ export default function HeaderPrincipale() {
               alt=""
             />
           </button>
-
-          <button onClick={toggleMenu} className="Cabecera-button">
-            <svg
-              className="Cabecera-svg"
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              viewBox="0 0 16 16"
-            >
-              <path
-                fillRule="evenodd"
-                d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
-              />
-            </svg>
-          </button>
-          <div className={`Cabecera-nav ${menu ? "isActive" : ""}`}>
-            <div className=" navbar Cabecera-ul  Cabecera-li ">
-              <a className="a-navbar Cabecera-li " href="#Home">
-                ALFA ROBOTICA <span className="span-navbar"></span>
+          <div>
+            <div className=" navbar  ">
+              <a className="a-navbar" href="#Home">
+                ALFA ROBOTICA
               </a>
               <span className="span-navbar"></span>
             </div>
